9b3c4b8f7463d7b956c43ce5beb23202f64d4c09
louieyang <louieyang61@gmail.com>
2024-10-23 15:20:19 -0400

    “Rectangle.java”

---
+++ Rectangle.java
@@ -0,0 +1,40 @@
+
+public class RecException extends Exception {
+	String message;
+
+		public RecException(String message) {
+		this.message = message;
+	}
+}
+
+public class Rectangle {
+	Point A;
+	Point B;
+	Point C;
+	Point D;
+	
+	public Rectangle(Point A, Point B, Point C, Point D) throws RecException{
+		Point a = new Point(A.x-B.x, A.y-B.y);
+		Point b = new Point(C.x-D.x, C.y-D.y);
+		if (!(a == b && A.x * B.x + A.y * B.y == 0))
+			throw new RecException("Not a rectangle");
+		else
+			this.A = A;
+			this.B = B;
+			this.C = C;
+			this.D = D;
+	}
+
+	public double perimeter() {
+		return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A);
+	}
+
+	public boolean isSquare() {
+		double rightangle = A.x * B.x + A.y * B.y;
+		if (A.distance(B) == B.distance(C) && rightangle == 0)
+			return true;
+		else
+			return false;
+	}
+
+}
